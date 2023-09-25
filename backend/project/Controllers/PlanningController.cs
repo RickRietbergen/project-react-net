f9171c24c61d248e7c111bf7449c87245c042a45
Rick Rietbergen <rbps3.2016@gmail.com>
2023-09-25 22:03:48 +0200

    make PlanningController

---
+++ backend/project/Controllers/PlanningController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project.DataBase;
+
+namespace project.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlanningController : ControllerBase
+    {
+        private readonly DatabaseContext dataContext;
+        public PlanningController(DatabaseContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        //[HttpGet]
+        //public async Task<ActionResult> Get()
+        //{
+
+        //}
+    }
+}
