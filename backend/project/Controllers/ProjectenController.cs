7e68bf3a2fd9c4743fb3557e93a19cb0af7d3fa4
Rick Rietbergen <rbps3.2016@gmail.com>
2023-09-25 14:39:10 +0200

    create ProjectenController + function create project

---
+++ backend/project/Controllers/ProjectenController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project.DataBase;
+using project.Entities;
+using project.Models;
+
+namespace project.Controllers
+{
+    public class ProjectenController : Controller
+    {
+        private readonly DatabaseContext dataContext;
+
+        public ProjectenController(DatabaseContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpPost("ProjectCreate")]
+        public async Task<IActionResult> Post(ProjectCreateModel model)
+        {
+            var projectName = model.Name;
+
+            var newProject = new Project
+            {
+                Name = projectName,
+            };
+
+            await dataContext.Projects.AddAsync(newProject);
+            await dataContext.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
