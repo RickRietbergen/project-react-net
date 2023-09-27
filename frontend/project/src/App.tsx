742edb6f637407fe60d9a87367eb0807d33fc90a
Rick Rietbergen <rbps3.2016@gmail.com>
2023-09-27 20:13:27 +0200

    install mui + test
    @mui/material, @mui/icons-material, @mui/icons-material, @emotion/styled

--- frontend/project/src/App.tsx
+++ frontend/project/src/App.tsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-      
+      <Box
+        sx={{width: "100%", height: "100%"}}
+      >
+        <Typography>This is a test</Typography>
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
