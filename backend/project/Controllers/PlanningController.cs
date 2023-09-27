c35f949158f21451716dc2b4d5236dbc9ad28c88
Rick Rietbergen <rbps3.2016@gmail.com>
2023-09-27 18:09:30 +0200

    functie hoeveel uur een werknemer aan welke projecten werkt

--- backend/project/Controllers/PlanningController.cs
+++ backend/project/Controllers/PlanningController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using project.DataBase;
+using System.Globalization;
 
 namespace project.Controllers
 {
@@ -13,10 +15,34 @@ namespace project.Controllers
             this.dataContext = dataContext;
         }
 
-        //[HttpGet]
-        //public async Task<ActionResult> Get()
-        //{
+        [HttpGet]
+        public async Task<ActionResult> GetWeekWorkHours()
+        {
+            int currentWeek = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(DateTime.Now, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
 
-        //}
+            var employeeWorkHours = dataContext.Planning
+                .Include(p => p.Employee)
+                .Include(p => p.Project)
+                .Where(p => p.Week == currentWeek)
+                .GroupBy(r => new
+                {
+                    EmployeeId = r.Employee.Id,
+                    EmployeeName = r.Employee.Name
+                })
+                .Select(w => new
+                {
+                    w.Key.EmployeeId,
+                    w.Key.EmployeeName,
+                    Projects = w.Select(r => new
+                    {
+                        ProjectId = r.Project.Id,
+                        ProjectName = r.Project.Name,
+                        HoursWorked = r.Hours
+                    }).ToList()
+                })
+                .ToList();
+
+            return Ok(employeeWorkHours);
+        }
     }
 }
