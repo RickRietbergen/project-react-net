e63cab7036b36cd803cf1fdc8f6eda639cebd52b
Rick Rietbergen <rbps3.2016@gmail.com>
2023-09-28 12:33:20 +0200

    update functie get planning

--- backend/project/Controllers/PlanningController.cs
+++ backend/project/Controllers/PlanningController.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using project.DataBase;
 using System.Globalization;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace project.Controllers
 {
@@ -20,29 +22,19 @@ namespace project.Controllers
         {
             int currentWeek = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(DateTime.Now, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
 
-            var employeeWorkHours = dataContext.Planning
+            var employeeWorkHours = await dataContext.Planning
                 .Include(p => p.Employee)
                 .Include(p => p.Project)
-                .Where(p => p.Week == currentWeek)
-                .GroupBy(r => new
-                {
-                    EmployeeId = r.Employee.Id,
-                    EmployeeName = r.Employee.Name
-                })
-                .Select(w => new
-                {
-                    w.Key.EmployeeId,
-                    w.Key.EmployeeName,
-                    Projects = w.Select(r => new
-                    {
-                        ProjectId = r.Project.Id,
-                        ProjectName = r.Project.Name,
-                        HoursWorked = r.Hours
-                    }).ToList()
-                })
-                .ToList();
+                .Where(p => p.Week == currentWeek).ToListAsync();
 
-            return Ok(employeeWorkHours);
+            var options = new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve
+            };
+
+            var json = JsonSerializer.Serialize(employeeWorkHours, options);
+
+            return Ok(json);
         }
     }
 }
