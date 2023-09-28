c0d229c1b4686d9b7a782a619c292cb3b1da4ca6
Rick Rietbergen <rbps3.2016@gmail.com>
2023-09-28 13:33:52 +0200

    laat de week zien op planning

--- frontend/project/src/App.tsx
+++ frontend/project/src/App.tsx
@@ -1,25 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import "./App.css";
+import { API_URL } from "./components/constants";
 
 function App() {
+  const [planningData, setPlanningData] = useState(null);
+  const [currentweek, setCurrentWeek] = useState(null);
+
+  const fetchPlanning = () => {
+    fetch(`${API_URL}Planning`, { method: "GET" })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setCurrentWeek(data.$values[0].Week);
+        setPlanningData(data.$values);
+      })
+      .catch((error) => {
+        console.log("Error fetching planning data:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchPlanning();
+  }, []);
+
   return (
     <>
       <Box
         display={"flex"}
-        //justifyContent={"center"}
         alignItems={"center"}
         flexDirection={"column"}
-        sx={{width: "100%", height: "100vh"}}
+        sx={{ width: "100%", height: "100vh" }}
       >
         <Box
           display={"flex"}
           justifyContent={"center"}
           alignItems={"center"}
-          sx={{width: "100%", height: "20%"}}
+          sx={{ width: "100%", height: "20%" }}
         >
-          <Typography sx={{fontSize: 30, fontWeight: "bold"}}>Week</Typography>
+          <Typography sx={{ fontSize: 30, fontWeight: "bold" }}>
+            Week {currentweek}
+          </Typography>
+        </Box>
+
+        <Box
+          display={"flex"}
+          sx={{ width: "100%", height: "80%", backgroundColor: "lightgrey" }}
+        >
+          <Box
+            sx={{width: "15%", height: ""}}
+          >
+
+          </Box>
+
         </Box>
       </Box>
     </>
