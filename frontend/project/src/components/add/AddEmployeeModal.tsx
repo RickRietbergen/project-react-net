4c849affc6aaba3e0a9cb53a90f0e7fc55f582fc
Rick Rietbergen <rbps3.2016@gmail.com>
2023-09-30 19:14:48 +0200

    voeg inputs toe aan modal, maak functie voor EmployeeCreate

--- frontend/project/src/components/add/AddEmployeeModal.tsx
+++ frontend/project/src/components/add/AddEmployeeModal.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
-import { Box, Button, Typography, Paper, Modal } from "@mui/material";
+import { useState } from "react";
+import { Box, Button, Typography, Paper, Modal, Tooltip, TextField } from "@mui/material";
+import { API_URL } from "../links/constants";
 
 interface AddEmployeeModalProps {
   isOpen: boolean;
@@ -7,6 +8,53 @@ interface AddEmployeeModalProps {
 }
 
 const AddEmployeeModal: React.FC<AddEmployeeModalProps> = ({ isOpen, onClose }) => {
+    const [Name, setName] = useState("");
+    const [ContractHours, setContractHours] = useState("");
+
+    const [NameError, setNameError] = useState("");
+    const [ContractHoursError, setContractHoursError] = useState("");
+
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+
+    const fetchCreateEmployee = () => {
+        setButtonDisabled(true);
+
+        const nameValid = Name.length >= 3;
+        const contractHoursValid = ContractHours.length >= 1;
+
+        if (!nameValid)
+          setNameError("Please enter more than 3 characters");
+
+        if (!contractHoursValid)
+          setContractHoursError("Please enter more then 1 number");
+
+        if (Name && ContractHours) {
+            fetch(`${API_URL}Employee/EmployeeCreate`, {
+                method: "POST",
+                body: JSON.stringify({
+                    Name,
+                    ContractHours,
+                }),
+                headers: {
+					"Content-type": "application/json; charset=UTF-8",
+				},
+            }).then((res) => {
+                setButtonDisabled(false);
+                if (!res.ok) {
+                    return res.text().then((text) => {
+                      console.log(text);
+                    });
+                }
+                window.location.href = "/employees";
+            })
+            .catch((error) => {
+                console.error("Fetch error:", error);
+            });
+        } else {
+            setButtonDisabled(false);
+        }
+    }
+
     return (
       <Modal
         open={isOpen}
@@ -28,9 +76,60 @@ const AddEmployeeModal: React.FC<AddEmployeeModalProps> = ({ isOpen, onClose }) 
           <Box
             display={"flex"}
             width={"100%"}
+            height={"15%"}
             justifyContent={"space-between"}
             alignItems={"center"}
           >
+            <Typography variant="h3">Add Employee</Typography>
+            <Tooltip title="Close Modal">
+              <Button onClick={onClose}>
+                <img
+                  src="../src/assets/close.png"
+                  alt="close modal"
+                  className="img"
+                />
+              </Button>
+            </Tooltip>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              width: "100%",
+              height: "85%",
+            }}
+          >
+            <TextField
+              label="Name"
+              helperText={NameError ? <Typography color="error">{NameError}</Typography> : null}
+              sx={{ marginTop: "7.5%" }}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            ></TextField>
+
+            <TextField
+              label="Contract Hours"
+              helperText={ContractHoursError ? <Typography color="error">{ContractHoursError}</Typography> : null}
+              sx={{ marginTop: "4%" }}
+              onChange={(e) => {
+                setContractHours(e.target.value);
+              }}
+            ></TextField>
+
+            <Tooltip title="Add Employee">
+              <Button
+                disabled={buttonDisabled}
+                variant="outlined"
+                color="primary"
+                sx={{ marginTop: "4%" }}
+                onClick={fetchCreateEmployee}
+              >
+                Add Employee
+              </Button>
+            </Tooltip>
           </Box>
         </Paper>
       </Modal>
