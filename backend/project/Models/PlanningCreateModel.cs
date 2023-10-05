81a5aa309decafd27d4a66ee4d4d6ea870847dff
Rick Rietbergen <rbps3.2016@gmail.com>
2023-10-05 10:27:23 +0200

    create model voor PlanningCreate

---
+++ backend/project/Models/PlanningCreateModel.cs
@@ -0,0 +1,10 @@
+﻿namespace project.Models
+{
+    public class PlanningCreateModel
+    {
+        public int Week { get; set; }
+        public int Hours { get; set; }
+        public int ProjectId { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
