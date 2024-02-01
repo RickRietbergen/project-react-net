1fb8cbd8b48fc0977b55f1b9bbdf362089f40cc5
Rick Rietbergen <rbps3.2016@gmail.com>
2024-02-01 14:00:34 +0100

    update-database

--- backend/project/Migrations/20240201124814_PasswordHash.Designer.cs
+++ backend/project/Migrations/20240201124814_PasswordHash.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using project.DataBase;
 
@@ -10,9 +11,11 @@ using project.DataBase;
 namespace project.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240201124814_PasswordHash")]
+    partial class PasswordHash
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- backend/project/Program.cs
+++ backend/project/Program.cs
@@ -38,7 +38,6 @@ namespace project
 
             app.UseAuthorization();
 
-
             app.MapControllers();
 
             app.Run();
